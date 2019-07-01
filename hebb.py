8cb9803e3e69e9a2e1c1f9d5c9cba39c34845b95
”Elias <clear@EliasRubin.localdomain>
2019-07-01 02:27:37 -0700

    Finished Program

--- hebb.py
+++ hebb.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
-import csv
+import json
+import argparse
+import sys
 
-print(sys.argv[1])
+
+weight = [];
+bias = 0;
+
+
+if len(sys.argv) == 1:
+    print('Error need the file')
+    sys.exit(1)
+
+try:
+    with open(sys.argv[1]) as jsf:
+        data = json.load(jsf)
+        i = 0;
+        for p in data['training']:
+            weight.append(p[0] + p[1])
+            weight[i] = weight[i] * data['target'][i]
+            bias = bias + data['target'][i]
+            print('Weight ' + str(i) + ': ' + str(weight[i]))
+            i = i + 1
+        jsf.close()
+except FileNotFoundError:
+    print('404 FILE NOT FOUND')
+    exit(1)
+
+print('Bias: ' + str(bias))
+
+
